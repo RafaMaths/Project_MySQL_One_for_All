e869d362a67dc3a4a49dbf0d867bb6780f86efab
RafaMaths <rmathscoach@gmail.com>
2021-08-03 19:22:41 -0300

    CHALLENGE 6: FIX alias

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
 CREATE VIEW faturamento_atual AS
-SELECT MIN(p.valor_plano) AS `faturamento_minimo`, MAX(p.valor_plano) AS `faturamento_maximo`, ROUND((SUM(p.valor_plano) / count(u.usuario_id)), 2) AS `faturamento_medio`,  SUM(p.valor_plano) AS `faturamento_total` FROM plano p
+SELECT MIN(p.valor_plano) AS faturamento_minimo, MAX(p.valor_plano) AS `faturamento_maximo`, ROUND((SUM(p.valor_plano) / count(u.usuario_id)), 2) AS `faturamento_medio`,  SUM(p.valor_plano) AS `faturamento_total` FROM plano p
 INNER JOIN usuario u
 ON p.plano_id = u.plano_id;
