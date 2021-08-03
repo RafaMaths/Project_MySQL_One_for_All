5491bb1db2db3bbe0bda8d5f145458eee4418929
RafaMaths <rmathscoach@gmail.com>
2021-08-03 19:21:01 -0300

    CHALLENGE 6: FIX average

--- desafio6.sql
+++ desafio6.sql
@@ -1,2 +1,4 @@
 CREATE VIEW faturamento_atual AS
-SELECT MIN(valor_plano) AS `faturamento_minimo`, MAX(valor_plano) AS `faturamento_maximo`, ROUND((SUM(valor_plano) / 4), 2) AS `faturamento_medio`,  SUM(valor_plano) AS `faturamento_total` FROM plano;
+SELECT MIN(p.valor_plano) AS `faturamento_minimo`, MAX(p.valor_plano) AS `faturamento_maximo`, ROUND((SUM(p.valor_plano) / count(u.usuario_id)), 2) AS `faturamento_medio`,  SUM(p.valor_plano) AS `faturamento_total` FROM plano p
+INNER JOIN usuario u
+ON p.plano_id = u.plano_id;
