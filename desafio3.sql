2b3d5ef397c9386b1de6f20ffb3c71848aea5038
RafaMaths <rmathscoach@gmail.com>
2021-08-03 18:48:53 -0300

    CHALLENGE 3: DONE

---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT u.nome_usuario AS usuario,  c.nome_cancao AS nome FROM usuario u
+INNER JOIN historico h
+ON u.usuario_id = h.usuario_id
+INNER JOIN cancao c
+ON c.cancao_id = h.cancao_id
+ORDER BY u.nome_usuario, c.nome_cancao;
