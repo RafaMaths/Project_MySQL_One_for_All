d32ceb8e4d54898e2c767def1bbf66a9cfa0f3bb
RafaMaths <rmathscoach@gmail.com>
2021-08-03 18:15:51 -0300

    CHALLENGE 2: DONE

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,4 @@
--- CREATE VIEW estatisticas_musicais AS
-SELECT count(c.cancao_id) AS `cancoes`, count(ar.artista_id) AS `artistas`, count(al.album_id) FROM 
-cancao c
+CREATE VIEW estatisticas_musicais AS
+SELECT count(c.nome_cancao) AS `cancoes`, count(DISTINCT ar.artista_id) AS `artistas`, count(DISTINCT c.album_id) AS `albuns` FROM cancao c
 INNER JOIN artista ar
-ON c.artista_id = ar.artista_id
-INNER JOIN album al
-ON ar.artista_id = al.artista_id;
+ON c.artista_id = ar.artista_id;
