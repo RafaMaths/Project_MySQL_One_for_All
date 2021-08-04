a7237075cc549b3b49bbc2142d3efdb3f1a95cf3
RafaMaths <rmathscoach@gmail.com>
2021-08-03 21:08:34 -0300

    CHALLENGE 9: DONE

--- desafio8.sql
+++ desafio8.sql
@@ -1,31 +1,15 @@
- DELIMITER $$
+-- DELIMITER $$
+-- CREATE TRIGGER trigger_usuario_delete AFTER DELETE ON usuario
+-- FOR EACH ROW
+-- BEGIN
+--   DELETE FROM seguidor
+--     WHERE seguidor.id_usuario = usuario.id_usuario,
+--   DELETE FROM historico
+--     WHERE historico.id_usuario = usuario.id_usuario;
+-- END $$
+-- DELIMITER ;
 
- CREATE TRIGGER trigger_usuario_delete
-  AFTER DELETE ON usuario --posso dizer que minha trigger será executada antes (BEFORE) ou depois (AFTER) de um evento
- FOR EACH ROW
- BEGIN 
-  SET NEW.status_postagem = 'Aguardando Aprovação'
-  SET NEW.data_abertura = NOW();
- END $$
+-- DELETE FROM SpotifyClone.usuario
+-- WHERE usuario_id = 1;
 
- DELIMITER ;
-
- Para uma ação de Insert, Update ou Delete eu coloco um gatilho para disparar;
-
- DELIMITER $$
-
- CREATE TRIGGER trigger_postagem_insert
-  BEFORE INSERT ON postagem --posso dizer que minha trigger será executada antes (BEFORE) ou depois (AFTER) de um evento
- FOR EACH ROW
- BEGIN 
-  SET NEW.status_postagem = 'Aguardando Aprovação'
-  SET NEW.data_abertura = NOW();
- END $$
-
- DELIMITER ;
-
- > VER RESULTADO
-
- INSERT INTO perfil(saldo) VALUES (2500);
-
-SELECT * FROM perfil;
+-- DROP TRIGGER trigger_usuario_delete;
---
+++ desafio9.sql
@@ -0,0 +1,12 @@
+DELIMITER $$;
+CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(50))
+BEGIN
+SELECT a.nome_artista AS `artista`, al.nome_album AS `album`  FROM artista a
+INNER JOIN album al
+ON a.artista_id = al.artista_id
+WHERE a.nome_artista = nome_artista
+ORDER BY album;
+END $$;
+DELIMITER ; 
+CALL albuns_do_artista('Walter Phoenix');
+  
