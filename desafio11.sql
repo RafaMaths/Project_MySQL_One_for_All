2046f590ea26f07f56f1f2643d5f67f9ef7e983d
RafaMaths <rmathscoach@gmail.com>
2021-08-03 21:59:25 -0300

    CHALLENGE 10: FIX coment

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
-SELECT c.nome_cancao AS 'nome', count(h.usuario_id) AS 'reproducoes' FROM cancao c
-INNER JOIN historico h
-ON c.cancao_id = h.cancao_id
-INNER JOIN usuario u
-ON 
-WHERE h.usuario_id <> ;
+-- SELECT c.nome_cancao AS 'nome', count(h.usuario_id) AS 'reproducoes' FROM cancao c
+-- INNER JOIN historico h
+-- ON c.cancao_id = h.cancao_id
+-- INNER JOIN usuario u
+-- ON 
+-- WHERE h.usuario_id <> ;
