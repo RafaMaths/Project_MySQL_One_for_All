bffea3b0d0c0494823820cded5ac2ed652305895
RafaMaths <rmathscoach@gmail.com>
2021-08-03 22:04:10 -0300

    CHALLENGE 11: DONE

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,9 @@
--- SELECT c.nome_cancao AS 'nome', count(h.usuario_id) AS 'reproducoes' FROM cancao c
--- INNER JOIN historico h
--- ON c.cancao_id = h.cancao_id
--- INNER JOIN usuario u
--- ON 
--- WHERE h.usuario_id <> ;
+CREATE VIEW cancoes_premium AS
+SELECT c.nome_cancao AS 'nome', count(h.usuario_id) AS 'reproducoes' FROM cancao c
+INNER JOIN historico h
+ON c.cancao_id = h.cancao_id
+INNER JOIN usuario u
+ON h.usuario_id = u.usuario_id
+WHERE u.plano_id <> 1
+GROUP BY c.nome_cancao
+ORDER BY c.nome_cancao;
