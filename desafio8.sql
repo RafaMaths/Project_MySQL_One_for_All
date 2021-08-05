892434d97defcbb15fb2d056a256e28cb92d04b4
RafaMaths <rmathscoach@gmail.com>
2021-08-04 21:29:41 -0300

    CHALLENGE 8: DONE

--- desafio8.sql
+++ desafio8.sql
@@ -1,15 +1,10 @@
--- DELIMITER $$
--- CREATE TRIGGER trigger_usuario_delete AFTER DELETE ON usuario
--- FOR EACH ROW
--- BEGIN
---   DELETE FROM seguidor
---     WHERE seguidor.id_usuario = usuario.id_usuario,
---   DELETE FROM historico
---     WHERE historico.id_usuario = usuario.id_usuario;
--- END $$
--- DELIMITER ;
-
--- DELETE FROM SpotifyClone.usuario
--- WHERE usuario_id = 1;
-
--- DROP TRIGGER trigger_usuario_delete;
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete AFTER DELETE ON usuario
+FOR EACH ROW
+BEGIN
+DELETE FROM seguidor
+WHERE seguidor.id_usuario = OLD.id_usuario;
+DELETE FROM historico
+WHERE historico.id_usuario = OLD.id_usuario;
+END $$
+DELIMITER ;
